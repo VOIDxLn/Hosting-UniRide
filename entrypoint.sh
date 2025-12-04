45f0f65594721c6b4c7115af6ce6893153a0127a
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-04 17:31:32 -0500

    Add entrypoint script for Laravel application

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Esperar a que la base de datos responda
+echo "Esperando a la base de datos..."
+sleep 5
+
+php artisan migrate --force
+php artisan db:seed --force
+
+# Iniciar servidor Laravel
+php artisan serve --host=0.0.0.0 --port=10000
