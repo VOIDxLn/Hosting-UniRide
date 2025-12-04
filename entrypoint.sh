716221cc6da7a05ed75a76ee3cbf8d47010f655b
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-04 17:57:02 -0500

    Make database migration and seeding non-blocking

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 echo "Esperando a la base de datos..."
 sleep 5
 
-php artisan migrate --force
-php artisan db:seed --force
+php artisan migrate --force || true
+php artisan db:seed --force || true
 
 # Iniciar servidor Laravel
 php artisan serve --host=0.0.0.0 --port=10000
