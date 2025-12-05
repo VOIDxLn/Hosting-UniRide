fa8e57f58759b9a97176a059bad659261a7f0020
VOIDxLn <juandedsec@gmail.com>
2025-12-05 09:51:10 -0500

    correccion envio de correos

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -85,4 +86,9 @@ class User extends Authenticatable
         return $this->hasMany(Review::class, 'passenger_id');
     }
 
+    public function sendPasswordResetNotification($token)
+    {
+    // Usamos la notificación que creamos
+    $this->notify(new ResetPasswordNotification($token));
+    }
 }
