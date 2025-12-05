3e68e8d5c379a714ea21f389aac49794a2bf18e2
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-05 10:32:11 -0500

    Remove docblock from run method in RoleSeeder
    Removed unnecessary docblock for the run method.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -7,9 +7,6 @@ use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $roles = ['Admin', 'Conductor', 'Pasajero'];
