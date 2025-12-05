c2b8b127573595c6a52a6f02c01df25ccaa29888
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-05 10:32:29 -0500

    Clean up AdminUserSeeder by removing comments
    Removed comments and unnecessary docblock from AdminUserSeeder.

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -9,12 +9,8 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Buscar o crear el usuario administrador
         $admin = User::firstOrCreate(
             ['email' => 'admin@uniride.com'],
             [
@@ -23,10 +19,8 @@ class AdminUserSeeder extends Seeder
             ]
         );
 
-        // Buscar el rol Admin
         $adminRole = Role::where('name', 'Admin')->first();
 
-        // Asignar el rol
         if ($adminRole) {
             $admin->roles()->syncWithoutDetaching([$adminRole->id]);
         }
