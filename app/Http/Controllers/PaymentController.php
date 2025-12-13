cb32db71d362e461436790edb135fbc87217b0c6
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-12 23:01:07 -0500

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,15 +17,16 @@ class PaymentController extends Controller
     public function checkout($trip_id)
     {
 
-        dd(
+        dd([
         'ENV' => env('STRIPE_SECRET'),
         'CONFIG' => config('services.stripe.secret'),
-        'SERVICES' => config('services')
-        );
+        'SERVICES' => config('services'),
+        ]);
     
         $trip = Trip::findOrFail($trip_id);
     
         Stripe::setApiKey(config('services.stripe.secret'));
+
             
         $trip = Trip::findOrFail($trip_id);
 
