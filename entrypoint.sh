dc7ba531bcfc2d4f57b79abb5dd464897d0d3388
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-13 07:00:29 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,12 @@ set -e
 
 cd /var/www/html
 
-echo "Asignando permisos..."
 chmod -R 775 storage bootstrap/cache
 
-echo "Esperando a la base de datos..."
 sleep 5
 
-echo "Ejecutando migraciones..."
-php artisan migrate --force || true
+php artisan migrate --force
+php artisan db:seed --class=RoleSeeder --force
+php artisan db:seed --class=AdminUserSeeder --force
 
-echo "Ejecutando seeders..."
-php artisan db:seed --force || true
-
-echo "Iniciando servidor Laravel..."
 php artisan serve --host=0.0.0.0 --port=10000
