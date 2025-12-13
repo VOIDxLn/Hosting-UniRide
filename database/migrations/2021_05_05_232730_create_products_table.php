1c56b1e9db4f46c12db6ce36bfc7a627f9f1eab9
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-13 07:28:16 -0500

    Update 2021_05_05_232730_create_products_table.php

--- database/migrations/2021_05_05_232730_create_products_table.php
+++ database/migrations/2021_05_05_232730_create_products_table.php
@@ -6,31 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('products', function(Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->float('unit_price');
-            $table->integer('quantity');
-            $table->float('total_cost');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('products')) {
+            Schema::create('products', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->text('description')->nullable();
+                $table->float('unit_price');
+                $table->integer('quantity');
+                $table->float('total_cost');
+                $table->timestamps();
+            });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('products');
     }
 }
+
