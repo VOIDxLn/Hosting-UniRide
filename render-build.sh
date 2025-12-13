ed010ab7daf665b2aac79904da9c8c022f5ab37b
VOIDxLn <131388379+VOIDxLn@users.noreply.github.com>
2025-12-12 23:17:19 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -7,7 +7,6 @@ composer install --no-dev --optimize-autoloader
 # 2. Generar la llave de la aplicación (solo si no está ya presente y es necesaria)
 # Si tu APP_KEY ya está configurada en Render, puedes omitir esta línea,
 # pero es seguro dejarla si la necesitas.
-php artisan key:generate
 
 # 3. LIMPIEZA DE CACHÉ (CRÍTICA)
 # Esto obliga a Laravel a leer los archivos LoginController.php y User.php corregidos
